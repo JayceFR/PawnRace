6d09ad9d7ac6005056de8ab8e68259a2c9bb47fc
JayceFR <jaycejefferson.vicious@gmail.com>
2025-01-10 13:35:58 +0000

    added pawns

--- app/src/main/java/com/jaycefr/pawnrace/MainActivity.kt
+++ app/src/main/java/com/jaycefr/pawnrace/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.jaycefr.pawnrace
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,8 +23,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jaycefr.pawnrace.ui.theme.PawnRaceTheme
@@ -36,9 +42,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             PawnRaceTheme {
                 Column(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.Black),
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
+                    verticalArrangement = Arrangement.Center
                 ) {
                     board(
                         boxWidthPx = boxWidth,
@@ -56,21 +64,45 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun board(boxWidthPx : Int, colors : List<Color> = listOf(Color.Black, Color.White)){
     val boxWidth = with(LocalDensity.current) { boxWidthPx.toDp() }
-    for (x in 0..7) {
-        Row(
-
-        ) {
-            for (y in 0..7) {
-                Box(
-                    modifier = Modifier
-                        .width(boxWidth)
-                        .height(boxWidth)
-                        .background(colors[(x + y) % 2])
-                )
+    Column(
+        Modifier.border(2.dp, Color(118, 150, 86, 89))
+    ){
+        for (x in 0..7) {
+            Row(
+            ){
+                for (y in 0..7) {
+                    Box(
+                        modifier = Modifier
+                            .width(boxWidth)
+                            .height(boxWidth)
+                            .background(colors[(x + y) % 2]),
+                        contentAlignment = Alignment.Center
+                    ){
+                        BlackPawn()
+                    }
+                }
             }
         }
     }
 }
+
+@Composable
+fun BlackPawn(){
+    Image(
+        painter = painterResource(id = R.drawable.pawnblack),
+        contentDescription = "BlackPawn",
+        Modifier.scale(4f)
+    )
+}
+
+@Composable
+fun WhitePawn(){
+    Image(
+        painter = painterResource(id = R.drawable.pawnwhite),
+        contentDescription = "WhitePawn"
+    )
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
