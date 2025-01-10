51e4cfec5e847bcd4a32be19f715899cfbbbdbbe
JayceFR <jaycejefferson.vicious@gmail.com>
2025-01-10 12:56:09 +0000

    drew the emty board

--- app/src/main/java/com/jaycefr/pawnrace/MainActivity.kt
+++ app/src/main/java/com/jaycefr/pawnrace/MainActivity.kt
@@ -1,28 +1,51 @@
 package com.jaycefr.pawnrace
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jaycefr.pawnrace.ui.theme.PawnRaceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val displayMatrix = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMatrix)
+        val boxWidth : Int = (displayMatrix.widthPixels - 100) / 8
         setContent {
             PawnRaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    board(
+                        boxWidthPx = boxWidth,
+                        colors = listOf(
+                            Color(118,150,86),
+                            Color(238,238,210)
+                        )
                     )
                 }
             }
@@ -30,6 +53,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun board(boxWidthPx : Int, colors : List<Color> = listOf(Color.Black, Color.White)){
+    val boxWidth = with(LocalDensity.current) { boxWidthPx.toDp() }
+    for (x in 0..7) {
+        Row(
+
+        ) {
+            for (y in 0..7) {
+                Box(
+                    modifier = Modifier
+                        .width(boxWidth)
+                        .height(boxWidth)
+                        .background(colors[(x + y) % 2])
+                )
+            }
+        }
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
